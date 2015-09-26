dbf71360aa6dfd0b7ad440d46cbd2cb356f42235
muupu <once645@gmail.com>
2015-09-26 14:31:12 +0800

    add conhello.cpp

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# concurrency-
+# cppconcurrency
---
+++ conhello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <thread>  // 管理线程的函数和类
+
+void hello()
+{
+	std::cout << "Hello Concurrency World" << std::endl;
+}
+
+int main()
+{
+	std::thread t(hello);
+	t.join(); // 调用线程等待t线程完成后，再继续运行下去
+}
